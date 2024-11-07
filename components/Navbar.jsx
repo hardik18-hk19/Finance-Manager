89a9a9d13b9413157e31cf15d3d6b701d6d5972c
Hardik Agarwal <hardikagarwal9308@gmail.com>
2024-11-07 22:19:18 +0530

    Added more tabs in Navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,7 +3,13 @@ import React from "react";
 import logo from "../public/file.png";
 import { Button } from "./ui/button";
 import Link from "next/link";
-import { HomeIcon, LayoutDashboardIcon, ScanBarcode } from "lucide-react";
+import {
+  HomeIcon,
+  InfoIcon,
+  LayoutDashboardIcon,
+  ScanBarcode,
+  WrenchIcon,
+} from "lucide-react";
 
 const Navbar = () => {
   // Links
@@ -11,7 +17,7 @@ const Navbar = () => {
     {
       label: "Home",
       key: "1",
-      href: "/home",
+      href: "/",
       icon: <HomeIcon className="w-5 h-5" />,
     },
     {
@@ -26,6 +32,18 @@ const Navbar = () => {
       href: "/transactions",
       icon: <ScanBarcode className="w-5 h-5" />,
     },
+    {
+      label: "Tools",
+      key: "4",
+      href: "/tools",
+      icon: <WrenchIcon className="w-5 h-5" />,
+    },
+    {
+      label: "About",
+      key: "5",
+      href: "/about",
+      icon: <InfoIcon className="w-5 h-5" />,
+    },
   ];
 
   return (
@@ -33,12 +51,12 @@ const Navbar = () => {
       <div className="pl-8">
         <Image src={logo} height={75} width={75} alt="logo" />
       </div>
-      <div className="flex items-center justify-between gap-8">
+      <div className="flex items-center  justify-between gap-8 pt-2">
         {links.map(({ label, href, key, icon }) => (
           <Link
             href={href}
             key={key}
-            className="flex gap-2 relative group hover:text-white transition duration-300"
+            className="flex gap-2 relative group hover:text-white hover:text-xl transition duration-300"
           >
             {icon}
             {label}
